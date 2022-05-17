41f3c24ee62abe7a88b4cb1faa6db4740ec3a15b
Liav <oneandonlyliav@gmail.com>
2022-05-17 17:16:29 -0400

    add global variable module

---
+++ src/modules/globals.js
@@ -0,0 +1,3 @@
+export const body = document.getElementsByTagName('body')[0];
+export const header = document.getElementsByTagName('header')[0];
+export const main = document.getElementsByClassName('main')[0];
