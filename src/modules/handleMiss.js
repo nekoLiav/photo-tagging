fab73864ad40ddb4d9fc1b965679addd0ba7f960
Liav <oneandonlyliav@gmail.com>
2022-05-18 22:10:30 -0400

    add miss UI feedback

--- src/modules/handleMiss.js
+++ src/modules/handleMiss.js
@@ -1,3 +1,18 @@
-const handleMiss = () => {};
+import { main } from './globals';
+
+const handleMiss = (menuClickEvent) => {
+  const miss = document.createElement('p');
+  miss.className = 'miss';
+  miss.textContent = 'Keep looking!';
+
+  miss.style.left = `${menuClickEvent.x.toString()}px`;
+  miss.style.top = `${menuClickEvent.y.toString()}px`;
+
+  main.append(miss);
+
+  setTimeout(() => {
+    miss.remove();
+  }, 2000);
+};
 
 export default handleMiss;
