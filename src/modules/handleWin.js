7b4599bcba238c5712cd3aa07e8ce8d5eade017f
Liav <oneandonlyliav@gmail.com>
2022-05-19 14:45:20 -0400

    update win handling

--- src/modules/handleWin.js
+++ src/modules/handleWin.js
@@ -1,12 +1,28 @@
 import { main, gameState } from './globals';
 
 const handleWin = () => {
-  const win = document.createElement('div');
-  win.className = 'win';
-  win.textContent = 'Got the W.';
+  const winModal = document.createElement('div');
+  winModal.className = 'win-modal';
+
+  const winMessage = document.createElement('p');
+  winMessage.textContent = `You found them in ${gameState.timer} seconds! Submit your name to appear on the leaderboard.`;
+
+  const nameInput = document.createElement('input');
+  nameInput.className = 'name-input';
+  nameInput.type = 'text';
+
+  const submitButton = document.createElement('button');
+  submitButton.className = 'submit-button';
+  submitButton.textContent = 'Submit';
+
+  submitButton.addEventListener('click', () => {
+    console.log(nameInput.value);
+  });
+
   gameState.gameOver = true;
 
-  main.append(win);
+  winModal.append(winMessage, nameInput, submitButton);
+  main.append(winModal);
 };
 
 export default handleWin;
