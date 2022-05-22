5b15629a8fb0325d8599da68c38979513b70f858
Liav <oneandonlyliav@gmail.com>
2022-05-22 00:36:28 -0400

    improve leaderboard updating method

--- src/firebase/handleLbAdd.js
+++ src/firebase/handleLbAdd.js
@@ -1,19 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, updateDoc, arrayUnion } from 'firebase/firestore';
 import firebaseConfig from './firebase-config';
-import { mapConfig } from '../modules/globals';
 
 const firebaseLbAdd = async (name, date, time, map) => {
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-  const metadata = mapConfig.filter(
-    (element) => Object.keys(element)[0] === map
-  );
   await updateDoc(doc(db, 'leaderboards', 'maps'), {
-    [map]: {
-      leaders: arrayUnion({ name, date, time }),
-      metadata: { name: metadata[0][map].name },
-    },
+    [`${map}.leaders`]: arrayUnion({ name, date, time }),
   });
 };
 
